92d23d3e3ca26568e92ff28359ff2b83ba4de9cf
LadyAlena <107322107+LadyAlena@users.noreply.github.com>
2023-01-17 15:05:42 +0300

    2 раза объявлена функция print_point

--- task1/source/main1.cpp
+++ task1/source/main1.cpp
@@ -9,10 +9,7 @@ struct point {
 	}
 };
 
-void print_point(const point& point_object) {
-	std::cout << "x:" << point_object.m_x << ", y: "
-		<< point_object.m_y << std::endl;
-}
+void print_point(const point& point_object);
 
 int Main()
 {
